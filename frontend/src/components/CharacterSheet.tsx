1e175f26b9dd181338f566008c2bdab27e32855e
Chris Marston <chris.scott.marston@gmail.com>
2024-07-31 09:00:23 -0400

    Added more fields and cleaned up existing fields.

--- frontend/src/components/CharacterSheet.tsx
+++ frontend/src/components/CharacterSheet.tsx
@@ -20,10 +20,13 @@ type Character = {
     item_slots: number
     armor_class: number
     alignment: number
+    speed: number
 }
 
+// TODO: Add placeholder text to all of the input fields. Lookup how to do this proeprly in React. I don't think giving things an initial value like I am doing is the right way.
+
 const DEFAULT_CHARACTER_DATA = {
-    name: "",
+    name: "name",
     id: "",
     inventory: "",
     character_description: "",
@@ -39,33 +42,55 @@ const DEFAULT_CHARACTER_DATA = {
     max_hit_points: 0,
     item_slots: 0,
     armor_class: 0,
-    alignment: 0
+    alignment: 0,
+    speed: 30
 }
 
 function CharacterSheetBox({ title, children }: any)
 {
     return (
-        <div className="p-1 mb-2 w-1/2 max-w-screen-sm border-white border-2">
+        <div className="p-1 mb-2 w-full max-w-screen-sm border-white border-2">
             {title && <p className="mb-2">{title}</p>}
             {children}
         </div>
     )
 }
 
-function CharacterSheetInput({ inputValue, labelText, characterDataKey, onChangeHandler, small }: any)
+function CharacterSheetInput({ inputValue, labelText, characterDataKey, onChangeHandler, long, className }: any)
 {
     return (
-        <>
-            {labelText && <label>{labelText}</label>}
+        <p className={className}>
+            {labelText && <label className="mr-2">{labelText}</label>}
             <input
                 type="text"
                 id="name"
                 name="name"
                 value={inputValue}
                 onChange={(event) => onChangeHandler(characterDataKey, event.target.value)}
-                className={`text-white bg-transparent focus:outline-none ${small && "w-5"}`}
+                className={`text-white bg-transparent focus:outline-none ${!long && "w-5"}`}
             />
-        </>
+        </p>
+    )
+}
+
+function HitPointsInput({ onChangeHandler, currentHitPoints, maxHitPoints }: any)
+{
+    const styles = "text-white bg-transparent focus: outline-none w-5";
+    return (
+        <label>
+            <span className="mr-2">HP:</span>
+            <input
+                className={styles}
+                value={currentHitPoints}
+                onChange={(event) => onChangeHandler("current_hit_points", event.target.value)}
+            />
+            <span className="mr-2">/</span>
+            <input
+                className={styles}
+                value={maxHitPoints}
+                onChange={(event) => onChangeHandler("max_hit_points", event.target.value)}
+            />
+        </label>
     )
 }
 
@@ -76,90 +101,28 @@ export default function CharacterSheet()
     return (
         <>
             <SiteHeader pageTitle="Character Sheet"/>
-            <main className="flex flex-col items-center">
+            <main className="flex flex-col items-center px-2">
                 <CharacterSheetBox>
-                    <CharacterSheetInput
-                        inputValue={character.name}
-                        characterDataKey="name"
-                        onChangeHandler={setCharacter}
-                    />
+                    <CharacterSheetInput inputValue={character.name} characterDataKey="name" onChangeHandler={setCharacter} long={true} />
                 </CharacterSheetBox>
 
                 <CharacterSheetBox title="Ability Scores">
-                    <p>
-                        <CharacterSheetInput
-                            labelText="HP: "
-                            inputValue={character.current_hit_points}
-                            characterDataKey="current_hit_points"
-                            onChangeHandler={setCharacter}
-                            small={true}
-                        />
-                        <span> / </span>
-                        <CharacterSheetInput
-                            inputValue={character.max_hit_points}
-                            characterDataKey="max_hit_points"
-                            onChangeHandler={setCharacter}
-                            small={true}
-                        />
-                    </p>
-                    <p>
-                        <CharacterSheetInput
-                            labelText="AC: "
-                            inputValue={character.armor_class}
-                            characterDataKey="armor_class"
-                            onChangeHandler={setCharacter}
-                        />
-                    </p>
-                    <br></br>
-                    {/* TODO: Make a new component for the ability scores that calcualtes and displays the modifier. */}
-                    <p>
-                        <CharacterSheetInput
-                            labelText="STR: "
-                            inputValue={character.str}
-                            characterDataKey="str"
-                            onChangeHandler={setCharacter}
-                        />
-                    </p>
-                    <p>
-                        <CharacterSheetInput
-                            labelText="DEX: "
-                            inputValue={character.dex}
-                            characterDataKey="dex"
-                            onChangeHandler={setCharacter}
-                        />
-                    </p>
-                    <p>
-                        <CharacterSheetInput
-                            labelText="CON: "
-                            inputValue={character.con}
-                            characterDataKey="con"
-                            onChangeHandler={setCharacter}
-                        />
-                    </p>
-                    <p>
-                        <CharacterSheetInput
-                            labelText="INT: "
-                            inputValue={character.int}
-                            characterDataKey="int"
-                            onChangeHandler={setCharacter}
-                        />
-                    </p>
-                    <p>
-                        <CharacterSheetInput
-                            labelText="WIS: "
-                            inputValue={character.wis}
-                            characterDataKey="wis"
-                            onChangeHandler={setCharacter}
-                        />
-                    </p>
-                    <p>
-                        <CharacterSheetInput
-                            labelText="CHA: "
-                            inputValue={character.cha}
-                            characterDataKey="cha"
-                            onChangeHandler={setCharacter}
-                        />
-                    </p>
+                    <HitPointsInput onChangeHandler={setCharacter} currentHitPoints={character.current_hit_points} maxHitPoints={character.max_hit_points} />
+                    <CharacterSheetInput labelText="AC:" inputValue={character.armor_class} characterDataKey="armor_class" onChangeHandler={setCharacter} />
+                    <div className="mt-5">
+                        {/* TODO: Make a new component for the ability scores that calcualtes and displays the modifier. Or at least add a place to enter the modifier. */}
+                        <CharacterSheetInput labelText="STR:" inputValue={character.str} characterDataKey="str" onChangeHandler={setCharacter} />
+                        <CharacterSheetInput labelText="DEX:" inputValue={character.dex} characterDataKey="dex" onChangeHandler={setCharacter} />
+                        <CharacterSheetInput labelText="CON:" inputValue={character.con} characterDataKey="con" onChangeHandler={setCharacter} />
+                        <CharacterSheetInput labelText="INT:" inputValue={character.int} characterDataKey="int" onChangeHandler={setCharacter} />
+                        <CharacterSheetInput labelText="WIS:" inputValue={character.wis} characterDataKey="wis" onChangeHandler={setCharacter} />
+                        <CharacterSheetInput labelText="CHA:" inputValue={character.cha} characterDataKey="cha" onChangeHandler={setCharacter} />
+                    </div>
+                    <div className="mt-5">
+                        <CharacterSheetInput labelText="Item Slots (10 + CON):" inputValue={character.item_slots} characterDataKey="item_slots" onChangeHandler={setCharacter} />
+                        {/* TODO: Add the "ft" units label to the end of this line. */}
+                        <CharacterSheetInput labelText="Speed:" inputValue={character.speed} characterDataKey="speed" onChangeHandler={setCharacter} />
+                    </div>
                 </CharacterSheetBox>
             </main>
         </>
